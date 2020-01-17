bc6b3ec14f29d021e8c9e0151a97fba37135674f
Daniel_praca <danielkaleta97@gmail.com>
2020-01-17 11:09:25 +0100

    Okazało się, że trzymanie servletów w innej paczce generuje przedziwne błędy

--- Turnieje/src/java/Turnieje/Servlets/CreateTournamentServlet.java
+++ Turnieje/src/java/Turnieje/Servlets/CreateTournamentServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Turnieje.Servlets.DanielKaleta;
+package Turnieje.Servlets;
 
 import java.io.IOException;
 import java.text.ParseException;
